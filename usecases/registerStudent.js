9a561dada78c33f15395ded20c3cbbd7db0daa0a
Alexandre Alves <alexandredias1995@gmail.com>
2018-11-30 19:23:28 -0200

    Fix student register; csv generate

--- usecases/registerStudent.js
+++ usecases/registerStudent.js
@@ -3,19 +3,20 @@ const Account = require('../domains/account');
 
 const registerStudent = (studentObj, user) => {
     let promises = [];
+
     if (Array.isArray(studentObj)) {
         students = studentObj.map(student => { 
             student.registeredBy = user; 
             return student;
         });
         promises = students.map(student => new Student(student).save());
+        return Promise.all(promises);
     } else {
         const student = studentObj;
         student.registeredBy = user;
-        promises.push(new Student(student).save());
+        return new Student(student).save();
     }
 
-    return Promise.all(promises);
 }
 
 module.exports = registerStudent
