a918dd6176aaf185bcbbd6adc1ea6f5b9a8a45d6
Alexandre Alves <alexandredias1995@gmail.com>
2018-12-15 15:08:32 -0200

    Fix xls headers

--- usecases/generateStudentsXls.js
+++ usecases/generateStudentsXls.js
@@ -1,10 +1,24 @@
 const json2xls = require('json2xls');
 const fs = require('fs');
+const renameKeys = require('rename-keys');
+
+const headers = {
+    "name": "Nome",
+    "email": "E-mail",
+    "phone": "Telefone",
+    "registeredBy": "Cadastrado Por:",
+    "liveNear": "Próximo de:"
+}
 
 const generateStudentsXls = (students) => {
+    const renamedStudents = students.map(student => renameKeys(student, (k, v) => {
+        return headers[k];
+    }));
     return new Promise((resolve, reject) => {
         try {
-            const xls = json2xls(students);
+            const xls = json2xls(renamedStudents, {
+                fields: ["Nome", "E-mail", "Telefone", "Cadastrado Por:", "Próximo de:"]
+            });
             const filename = "relatorio";
             const path = `tmp/xls/${filename}.xlsx`
             fs.writeFileSync(path, xls, 'binary');
